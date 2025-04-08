e800dd4457a30ce14bc8d168e82f4bc675773e1f
krishu073 <krishnapanjre2003@gmail.com>
2025-04-08 13:30:23 +0530

    Your message describing the update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,6 +8,8 @@ import ThemeButton from "./ThemeButton";
 import Menu from "./../public/assets/icons/menu.svg";
 import Close from "./../public/assets/icons/close.svg";
 import { slideIn } from "@/utils/motion";
+import avatar from "./../public/assets/kprofile.png";
+import logos from "./../public/assets/v.png";
 
 function Navbar() {
 	const [active, setActive] = useState("");
@@ -28,7 +30,7 @@ function Navbar() {
 				<div className="sm:w-[500px] sm:h-[500px] xs:w-[400px] xs:h-[400px] w-[200px] h-[200px] dark:bg-[#2b2b42d2] bg-[#e0eaf0] flex justify-center items-center backdrop-blur-sm backdrop-filter bg-opacity-80 rounded-md modal">
 					<div className="relative w-[80%] h-[80%] rounded-md">
 						<Image
-							src="/assets/avatar.png"
+							src={avatar}
 							alt="avatar"
 							fill={true}
 							sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 20vw"
